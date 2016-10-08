272da65f8dfe434e891c560bf5ddae6e2def791b
Chopping Machine <Chopping Machine>
2016-10-08 12:47:42 +0800

    Initial commit

---
+++ djnotes/notes/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Note(models.Model):
+	title = models.CharField(max_length=512, blank=True)
+	note = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.title
