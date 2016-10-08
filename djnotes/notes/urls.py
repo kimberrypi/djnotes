a8e5abf8c3856a342ddf4f47bfa26ea2bffc9f56
Chopping Machine <Chopping Machine>
2016-10-08 17:08:13 +0800

    Additional functions:  update, edit, delete notes

---
+++ djnotes/notes/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.Index.as_view(), name='index'),
+	url(r'edit/(?P<pk>\d+)/$', views.Update.as_view(), name='update'),
+	url(r'^delete/(?P<pk>\d+)/$', views.Delete.as_view(), name='delete'),
+]
--- djnotes/notes/views.py
+++ djnotes/notes/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+from .models import Note
+from .forms import NoteForm
+
+
+class Index(View):
+	template_name = 'notes/index.html'
+
+	def get_context_data(self):
+		context = {
+			'notes':Note.objects.order_by('-id'),
+		}
+		return context
+
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data()
+		return render(request, self.template_name, context)
+
+	def post(self, request, *args, **kwargs):
+		form = NoteForm(request.POST)
+		context = self.get_context_data()
+
+		if form.is_valid():
+			form.save()
+		return render(request, self.template_name, context)
+
+class Update(View):
+	template_name = 'notes/update.html'
+
+	def get_context_data(self, **kwargs):
+		context = {}
+		try:
+			note_id = kwargs.get('pk',0)
+			note = Note.objects.get(id=note_id)
+
+		except Note.DoesNotExist:
+			raise Http404
+
+		else:
+			context['note'] = note
+			return context
+
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		return render(request, self.template_name, context)
+
+	def post(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		form = NoteForm(request.POST, instance=context.get('note'))
+		if form.is_valid():
+			form.save()
+		return render(request, self.template_name, context)
+
+class Delete(Update):
+	template_name = 'notes/delete.html'
+
+	def post(self, request, *args, **kwargs):
+		context = self.get_context_data(**kwargs)
+		note = context.get('note')
+		note.delete()
+		return redirect('index')
+		
